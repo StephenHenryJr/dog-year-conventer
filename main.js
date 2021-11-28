19077c0005232f12c2a1b649fec8e73aaa9db956
BlackNimbus <shenryjr_85@hotmail.com>
2021-11-28 16:44:44 -0500

    my first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// My age reference
+let myAge = 36;
+//First 2 years of dog's life
+let earlyYears = 2;
+//
+earlyYears *= 10.5;
+
+// My age - the 2 first dog years
+let laterYears = myAge - 2;
+// Later years of dog's life
+laterYears *= 4;
+//
+console.log(earlyYears);
+console.log(laterYears);
+// Early and later years of dog life
+let myAgeInDogYears = earlyYears + laterYears;
+// my name in lowercase
+let myName = 'Stephen'.toUpperCase();
+//
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`);
