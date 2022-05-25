87d1d9cc7f438d3b7788e6fac562f94ed7aebae4
StephenHenryJr <shenryjr_85@hotmail.com>
2022-05-25 19:18:25 -0400

    HTML, and JS added for browser functionality.

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-// My age reference
-let myAge = 36;
-//First 2 years of dog's life
-let earlyYears = 2;
-//
-earlyYears *= 10.5;
+let submit = document.getElementById('submit');
+let input = document.getElementById('input');
 
-// My age - the 2 first dog years
-let laterYears = myAge - 2;
-// Later years of dog's life
-laterYears *= 4;
-//
-console.log(earlyYears);
-console.log(laterYears);
-// Early and later years of dog life
-let myAgeInDogYears = earlyYears + laterYears;
-// my name in lowercase
-let myName = 'Stephen'.toUpperCase();
-//
-console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`);
+let calculateDogYears = () => {
+
+    let myAge = input.value;
+
+    //First 2 years of dog's life
+    let earlyYears = 2;
+    //
+    earlyYears *= 10.5;
+
+    // My age - the 2 first dog years
+    let laterYears = myAge - 2;
+    // Later years of dog's life
+    laterYears *= 4;
+    //
+
+    // Early and later years of dog life
+    let myAgeInDogYears = earlyYears + laterYears;
+    //
+
+    if (input.value > 0) {
+        document.querySelector('p').innerHTML = `Hello there, you are exactly ${myAgeInDogYears} years old in dog years!`;
+    } else {
+        document.querySelector('p').innerHTML = `Please enter a valid number above 0!`
+    }
+}
+
+// calculateDogYears()
+submit.addEventListener('click', calculateDogYears)
